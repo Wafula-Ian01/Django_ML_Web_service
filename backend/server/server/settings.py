2bfa8d083d15817ab7d5cc94362451220a6a28ca
Wafula-Ian01 <ianwelmer01@gmail.com>
2024-03-06 10:37:19 +0300

    serializers, urls and views

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y@cb693)tk7xtj^e&j@h07tpqs)59&@_*mo&sqdg4%y_7*%3zo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
